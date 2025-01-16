8b95d30c2fe663e109cce9711dc456175e709067
Karina <>
2025-01-16 09:42:58 +0200

    Update avg.cpp

--- avg.cpp
+++ avg.cpp
@@ -6,3 +6,11 @@ int main()
     double a,b;
     cout << "1st number" << endl;
         cin >> a;
+    cout << "2nd number" << endl;
+        cin >> b;
+    double avg = (a+b)/2;
+    cout << avg << endl;
+
+    
+    return 0;
+}
